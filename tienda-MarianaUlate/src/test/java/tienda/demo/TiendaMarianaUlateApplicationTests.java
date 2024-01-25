786d34ed1fe5c9ab953aba6939a6a8f37a393859
mariux02 <133933830+mariux02@users.noreply.github.com>
2024-01-24 18:34:09 -0600

    creación de proyecto

---
+++ tienda-MarianaUlate/src/test/java/tienda/demo/TiendaMarianaUlateApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMarianaUlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
